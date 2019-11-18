92918cf4fbd0b162a136f42e8ad6d5b264b01648
Lars Beerepoot <lars.beerepoot@multisafepay.com>
2019-11-18 14:18:57 +0100

    pauze

--- static/js/scroll-menu.js
+++ static/js/scroll-menu.js
@@ -5,4 +5,5 @@ tocbot.init({
     contentSelector: '.js-toc-content',
     // Which headings to grab inside of the contentSelector element.
     headingSelector: 'h1, h2, h3, h4, h5',
+    scrollContainer: 300,
   });
