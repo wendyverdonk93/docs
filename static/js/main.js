64b2535744df15a6e65ba9a7e169acfd333a3ac1
Alex-MultiSafepay <alex.carletto@multisafepay.com>
2020-05-14 11:25:12 +0200

    DOCS: new-searchbar (#476)

--- static/js/main.js
+++ static/js/main.js
@@ -10,3 +10,12 @@
     })
   }
 
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var innerWidth = window.innerWidth;
+  if(innerWidth<768){
+    var element = document.querySelectorAll("#search-home #search-input")[0]; 
+    element.addEventListener('focus', function(){
+      $('html, body').animate({scrollTop: $(element).offset().top -90 }, 'slow');
+     });
+  }  
+});
